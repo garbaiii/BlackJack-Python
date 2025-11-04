b8c3d8fc7c0b1e84c23583cd75418c6801bfd147
Garbai Zoltan <garbai.zoltan17@icloud.com>
2025-11-04 09:51:07 +0100

    Optimized Imports

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 import sys
 import os
 import time
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 
 kartyak = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'] * 8
 lap = 0
